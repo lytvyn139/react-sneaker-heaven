5e3b1479e97c6db09b1477a1062e33b07d07dd3b
lytvyn139 <lytvyn.139@gmail.com>
2020-10-14 08:52:07 -0500

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return <div className='App'>S</div>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to sneaker heaven</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
