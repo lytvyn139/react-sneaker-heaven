2b40b91b9bfa09c616a407ec51d785a51c45dce3
lytvyn139 <lytvyn.139@gmail.com>
2020-10-14 10:09:38 -0500

    added images & product component

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Kyrie 6 N7',
+    image: '/images/kyrie.jpg',
+    description:
+      'Celebrate the proud history of Native Americans and Indigenous peoples in North America with the Nike Kyrie 6 N7.',
+    brand: 'Nike',
+    category: 'Basketball',
+    price: 130,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 12
+  },
+  {
+    _id: '2',
+    name: 'Nike Romaleos 4 AMP',
+    image: '/images/romaleos-4.jpg',
+    description:
+      'Designed for strength and stability, the Nike Romaleos 4 AMP features a supportive midsole and a wide, flat base.',
+    brand: 'Nike',
+    category: 'Training Shoe',
+    price: 210,
+    countInStock: 4,
+    rating: 4.2,
+    numReviews: 3
+  },
+  {
+    _id: '3',
+    name: 'KD13',
+    image: '/images/kd13.jpg',
+    description:
+      'In the game’s crucial moments, KD thrives. Defenders fear his unstoppable moves and opponents get shut down by his defensive prowess. ',
+    brand: 'Nike',
+    category: 'Basketball',
+    price: 160,
+    countInStock: 3,
+    rating: 5,
+    numReviews: 8
+  },
+  {
+    _id: '4',
+    name: 'Nike Air Max 97',
+    image: '/images/air-max97.jpg',
+    description:
+      'The Nike Air Max 97 reimagines an iconic running shoe with frightening Halloween colors and slimy detailing throughout.',
+    brand: 'Nike',
+    category: 'Casual',
+    price: 180,
+    countInStock: 16,
+    rating: 4.7,
+    numReviews: 44
+  },
+  {
+    _id: '5',
+    name: 'Nike ISPA Zoom Road Warrior',
+    image: '/images/ispa-zoom.jpg',
+    description:
+      'Today"s environment has complex problems that need sophisticated solutions. The Nike ISPA Zoom Road Warrior scavenges parts from your favorite footwear designs for a collective mix of materials in shoe form.',
+    brand: 'Nike',
+    category: 'Casual',
+    price: 500,
+    countInStock: 5,
+    rating: 3.5,
+    numReviews: 10
+  },
+  {
+    _id: '6',
+    name: 'NikeCourt Air Zoom Vapor Cage 4',
+    image: '/images/nikecourt-air.jpg',
+    description:
+      'The NikeCourt Air Zoom Vapor Cage 4 is innovated to withstand your toughest matches—thanks to an updated design that puts flexible, durable materials exactly where they"re needed the most.',
+    brand: 'Nike',
+    category: 'Tennis shoe',
+    price: 150,
+    countInStock: 1,
+    rating: 4.2,
+    numReviews: 2
+  }
+];
+
+export default products;
