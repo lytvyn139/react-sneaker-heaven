06af58af4b0d303411ad77c83461d1ea122e5662
lytvyn139 <lytvyn.139@gmail.com>
2020-10-15 10:27:24 -0500

    added custom error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
-
 import productRouter from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -17,6 +17,10 @@ app.get("/", (req, res) => {
 
 /* thats why path was removed in productRouter */
 app.use("/api/products", productRouter);
+
+// ERROR HANDLER MIDDLEWARE from middleware/errorMiddleware.js
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
