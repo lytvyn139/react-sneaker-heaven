ebfa0bc1d833af7454cf94d9921cafb329cbb86c
lytvyn139 <lytvyn.139@gmail.com>
2020-10-16 21:47:34 -0500

    card add/remove working

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -11,7 +11,7 @@ import {
   Button,
   Card
 } from 'react-bootstrap';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id;
@@ -27,12 +27,11 @@ const CartScreen = ({ match, location, history }) => {
   }, [dispatch, productId, qty]);
 
   const removeFromCartHandler = (id) => {
-    // dispatch(removeFromCart(id));
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
-    // history.push('/login?redirect=shipping');
-    console.log('remove');
+    history.push('/login?redirect=shipping');
   };
 
   return (
