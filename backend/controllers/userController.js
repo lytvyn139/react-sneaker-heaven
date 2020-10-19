45f3c7ffb83e841b73a706b7884b88ee35044853
lytvyn139 <lytvyn.139@gmail.com>
2020-10-19 10:16:23 -0500

    backend user update route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -54,7 +54,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-// (Get user profile)PRIVATE|GET|/api/users/loign
+// (Get user profile)PRIVATE|GET|/api/users/login
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
   if (user) {
@@ -70,4 +70,27 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser };
+// (Update user profile)PRIVATE|GET|/api/users/profile
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+    res.json;
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+export { authUser, getUserProfile, registerUser, updateUserProfile };
