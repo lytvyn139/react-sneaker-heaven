e0d40f137381ba8b93a37ddbd5e01fadbec0fed7
lytvyn139 <lytvyn.139@gmail.com>
2020-10-22 08:48:17 -0500

    added backend file upload

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
@@ -5,6 +6,8 @@ import connectDB from "./config/db.js";
 import productRouter from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -22,10 +25,14 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRouter);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // ERROR HANDLER MIDDLEWARE from middleware/errorMiddleware.js
 app.use(notFound);
